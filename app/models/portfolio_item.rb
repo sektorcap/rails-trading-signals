3cc3c69c8c53dea49918913c87c75a29b165b98d
Ettore Elio Caprella <ettoreelio.caprella@telecomitalia.it>
2018-08-01 10:47:57 +0200

    add validations for portfolio

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -3,6 +3,12 @@ class PortfolioItem < ApplicationRecord
   belongs_to :stock
   attr_accessor :stock_ids
 
+  validates :stock, presence: true
+  validates :stock_ids, presence: true
+  validates :quantity, presence: true
+  validates :avg_stock_value, presence: true
+  validates :sell_stock_value, presence: true, if: :closed?
+
   def sell_stock_value
     return self[:sell_stock_value] if closed?
     return stock.quotation if stock && stock.quotation
@@ -10,7 +16,7 @@ class PortfolioItem < ApplicationRecord
   end
 
   def profit_loss
-    (sell_stock_value-avg_stock_value)*quantity
+    (sell_stock_value-avg_stock_value)*quantity rescue 0
   end
 
   def closed?
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
   has_many :stock_signals, dependent: :destroy
+  has_many :portfolio_items, dependent: :destroy, inverse_of: :stock
   validates_presence_of :symbol
   validates_uniqueness_of :symbol
   validates_presence_of :name
